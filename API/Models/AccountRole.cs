1cde784ad976dfa7b2926ce387bc24b6563d06bf
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-21 18:32:06 +0700

    Create AccountRole.cs

---
+++ API/Models/AccountRole.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    [Table("tb_tr_account_roles")]
+    public class AccountRole : BaseEntity
+    {
+        [Column("account_guid")]
+        public Guid AccountGuid { get; set; }
+
+        [Column("role_guid")]
+        public Guid RoleGuid { get; set; }
+    }
+}
