0e5cf71bd5b80f3e77935e930ada74faa76182ee
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-21 16:29:00 +0700

    Update Education.cs

--- API/Models/Education.cs
+++ API/Models/Education.cs
@@ -4,12 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models
 {
     [Table("tb_m_educations")]
-    public class Education
+    public class Education : BaseEntity
     {
-        [Key]
-        [Column("guid")]
-        public Guid Guid { get; set; }
-
         [Column("major", TypeName = "nvarchar(100)")]
         public string Major { get; set; }
 
@@ -18,12 +14,6 @@ namespace API.Models
 
         [Column("gpa")]
         public double Gpa { get; set; }
-
-        [Column("created_date")]
-        public DateTime CrearedDate { get; set; }
-
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
 
         [Column("university_guid")]
         public Guid UniversityGuid { get; set; }
