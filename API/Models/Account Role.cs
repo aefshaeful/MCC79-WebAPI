7606d49833dbd0a9198eb7452dbe07db5f007ddc
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-21 16:28:13 +0700

    Update Account Role.cs

--- API/Models/Account Role.cs
+++ API/Models/Account Role.cs
@@ -3,18 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.Models
 {
-    public class Account_Role
+    [Table("tb_m_account_roles")]
+    public class Account_Role : BaseEntity
     {
-        [Key]
-        [Column("guid")]
-        public Guid Guid { get; set; }
-
-        [Column("created_date")]
-        public DateTime CreatedDate { get; set; }
-
-        [Column("modified_date")]
-        public DateTime ModifiedDate { get; set; }
-
         [Column("account_guid")]
         public Guid AccountGuid { get; set; }
 
