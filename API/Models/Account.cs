d467ef4f27dc63e623336158c65006238e312f0a
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-21 16:41:48 +0700

    Update Account.cs

--- API/Models/Account.cs
+++ API/Models/Account.cs
@@ -7,17 +7,17 @@ namespace API.Models
     [Table("tb_m_accounts")]
     public class Account : BaseEntity
     {
-        [Column("password", TypeName = "nvarchar(max)")]
+        [Column("password", TypeName = "nvarchar(255)")]
         public string Password { get; set; }
 
         [Column("is_deleted")]
-        public Boolean IsDeleted { get; set; }
+        public bool IsDeleted { get; set; }
 
         [Column("otp")]
         public int Otp { get; set; }
 
         [Column("is_used")]
-        public Boolean IsUsed { get; set; }
+        public bool IsUsed { get; set; }
 
         [Column("expired_time")]
         public DateTime ExpiredTime { get; set; }
