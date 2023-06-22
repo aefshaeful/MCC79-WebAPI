17a84764da519cd748891e64bb8c8e46d16b44ab
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 05:14:08 +0700

    Create EducationController.cs

---
+++ API/Controllers/EducationController.cs
@@ -0,0 +1,79 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/education")]
+    public class EducationController : ControllerBase
+    {
+
+        private readonly IEducationRepository repository;
+        public EducationController(IEducationRepository Repository)
+        {
+            repository = Repository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var education = repository.GetAll();
+
+            if (!education.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(education);
+        }
+
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var education = repository.GetByGuid(guid);
+            if (education is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(education);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Education education)
+        {
+            var createdEducation = repository.Create(education);
+            return Ok(createdEducation);
+        }
+
+
+        [HttpPut]
+        public IActionResult Update(Education education)
+        {
+            var isUpdated = repository.Update(education);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDeleted = repository.Delete(guid);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
