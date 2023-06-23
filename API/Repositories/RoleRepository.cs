d632e6fd4132a74d68ff8ac0aaa85a2b8ded83dd
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 10:55:28 +0700

    Create RoleRepository.cs

---
+++ API/Repositories/RoleRepository.cs
@@ -0,0 +1,74 @@
+﻿using API.Models;
+using API.Contracts;
+using API.Data;
+
+
+namespace API.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly BookingDbContext context;
+        public RoleRepository(BookingDbContext Context)
+        {
+            this.context = Context;
+        }
+
+        public ICollection<Role> GetAll()
+        {
+            return context.Set<Role>().ToList();
+        }
+
+        public Role GetByGuid(Guid guid)
+        {
+            return context.Set<Role>().Find(guid);
+        }
+
+        public Role Create(Role role)
+        {
+            try
+            {
+                context.Set<Role>().Add(role);
+                context.SaveChanges();
+                return role;
+            }
+            catch (Exception ex)
+            {
+                return new Role();
+            }
+        }
+
+        public bool Update(Role role)
+        {
+            try
+            {
+                context.Set<Role>().Update(role);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Guid guid)
+        {
+            try
+            {
+                var role = GetByGuid(guid);
+
+                if (role is null)
+                {
+                    return false;
+                }
+                context.Set<Role>().Remove(role);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
