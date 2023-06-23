5a11fa1b431cf5a4bbb3f5fc289fb9dd3f047818
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 09:01:08 +0700

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<BookingDbContext>(optionsAction:options => options
 builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
