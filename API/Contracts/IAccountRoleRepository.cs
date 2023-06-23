40ba9bfc843fd9d220446df34f4d09ceb2aeeddd
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 10:44:10 +0700

    Create IAccountRoleRepository.cs

---
+++ API/Contracts/IAccountRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+
+namespace API.Contracts
+{
+    public interface IAccountRoleRepository
+    {
+        ICollection<AccountRole> GetAll();
+        AccountRole GetByGuid(Guid guid);
+        AccountRole Create(AccountRole accountrole);
+        bool Update(AccountRole accountrole);
+        bool Delete(Guid guid);
+    }
+}
