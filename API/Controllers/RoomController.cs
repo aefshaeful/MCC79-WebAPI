af84bc1f40d937d4b3bdf5d718e91fad54f0a932
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 09:57:09 +0700

    Create RoomController.cs

---
+++ API/Controllers/RoomController.cs
@@ -0,0 +1,82 @@
+﻿using API.Models;
+using API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/room")]
+
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomRepository repository;
+        public RoomController(IRoomRepository Repository)
+        {
+            this.repository = Repository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var room = repository.GetAll();
+
+            if(!room.Any()) 
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid) 
+        { 
+            var room = repository.GetByGuid(guid);
+
+            if(room is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Room room) 
+        {
+            var createdRoom = repository.Create(room);
+            return Ok(createdRoom);
+        }
+
+
+        [HttpPut]
+        public IActionResult Update(Room room) 
+        {
+            var isUpdate = repository.Update(room);
+
+            if(!isUpdate) 
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDelete = repository.Delete(guid);
+
+            if(!isDelete)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
