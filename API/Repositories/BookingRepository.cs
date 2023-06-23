8ee8e3d0841ac611b00cac773497ddcfd3cc429c
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-23 09:01:12 +0700

    Create BookingRepository.cs

---
+++ API/Repositories/BookingRepository.cs
@@ -0,0 +1,73 @@
+﻿using API.Data;
+using API.Contracts;
+using API.Models;
+
+namespace API.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly BookingDbContext context;
+        public BookingRepository (BookingDbContext Context) 
+        {
+            this.context = Context;
+        }
+
+        public ICollection<Booking> GetAll() 
+        {
+            return context.Set<Booking>().ToList();
+        }
+
+        public Booking GetByGuid(Guid guid) 
+        {
+            return context.Set<Booking>().Find(guid);
+        }
+
+        public Booking Create(Booking booking) 
+        {
+            try
+            {
+                context.Set<Booking>().Add(booking);
+                context.SaveChanges();
+                return booking;
+            }
+            catch (Exception ex) 
+            {
+                return new Booking();
+            }
+        }
+
+        public bool Update(Booking booking) 
+        {
+            try
+            {
+                context.Set<Booking>().Update(booking);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Guid guid)
+        {
+            try
+            {
+                var booking = GetByGuid(guid);
+                
+                if (booking is null) 
+                {
+                    return false;
+                }
+                context.Set<Booking>().Remove(booking);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
