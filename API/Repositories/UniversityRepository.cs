b2c66fe39930c7ba8efff7ad73e7f3e9f8d57128
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 08:02:51 +0700

    Update UniversityRepository.cs

--- API/Repositories/UniversityRepository.cs
+++ API/Repositories/UniversityRepository.cs
@@ -5,75 +5,14 @@ using System.Linq.Expressions;
 
 namespace API.Repositories
 {
-    public class UniversityRepository : IUniversityRepository
+    public class UniversityRepository : GeneralRepository<University>, IUniversityRepository
     {
-        private readonly BookingDbContext context;
-        public UniversityRepository(BookingDbContext Context)
-        {
-            this.context = Context;
+        public UniversityRepository(BookingDbContext Context) : base(Context) 
+        { 
         }
-
-
-        public ICollection<University> GetAll()
+        public IEnumerable<University> GetByName(string name)
         {
-            return context.Set<University>().ToList();
-        }
-
-
-        public University? GetByGuid(Guid guid) 
-        {
-            return context.Set<University>().Find(guid);
-        }
-
-
-        public University Create(University university) 
-        {
-            try 
-            {
-                context.Set<University>().Add(university);
-                context.SaveChanges();
-                return university;
-            }
-            catch (Exception ex) 
-            {
-                return new University();
-            }
-        }
-
-
-        public bool Update(University university) 
-        {
-            try
-            {
-                context.Set<University>().Update(university);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var university = GetByGuid(guid);
-
-                if (university is null)
-                {
-                    return false;
-                }
-                context.Set<University>().Remove(university);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
+            return context.Set<University>().Where(university => university.Name.Contains(name));
         }
     }
 }
