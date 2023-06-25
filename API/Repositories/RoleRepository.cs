e48f7b9aa5d0e9ca77e0d9de365e3f75a490f518
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 08:33:38 +0700

    Update RoleRepository.cs

--- API/Repositories/RoleRepository.cs
+++ API/Repositories/RoleRepository.cs
@@ -5,70 +5,8 @@ using API.Data;
 
 namespace API.Repositories
 {
-    public class RoleRepository : IRoleRepository
+    public class RoleRepository : GeneralRepository<Role>, IRoleRepository
     {
-        private readonly BookingDbContext context;
-        public RoleRepository(BookingDbContext Context)
-        {
-            this.context = Context;
-        }
-
-        public ICollection<Role> GetAll()
-        {
-            return context.Set<Role>().ToList();
-        }
-
-        public Role? GetByGuid(Guid guid)
-        {
-            return context.Set<Role>().Find(guid);
-        }
-
-        public Role Create(Role role)
-        {
-            try
-            {
-                context.Set<Role>().Add(role);
-                context.SaveChanges();
-                return role;
-            }
-            catch (Exception ex)
-            {
-                return new Role();
-            }
-        }
-
-        public bool Update(Role role)
-        {
-            try
-            {
-                context.Set<Role>().Update(role);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var role = GetByGuid(guid);
-
-                if (role is null)
-                {
-                    return false;
-                }
-                context.Set<Role>().Remove(role);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
+        public RoleRepository(BookingDbContext Context) : base(Context) { }
     }
 }
