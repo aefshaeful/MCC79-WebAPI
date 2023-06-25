73094afffa07d6953d4db1abaa92caea19d7ad71
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 09:18:40 +0700

    Update UniversityController.cs

--- API/Controllers/UniversityController.cs
+++ API/Controllers/UniversityController.cs
@@ -7,73 +7,8 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/universities")]
-    public class UniversityController : ControllerBase
+    public class UniversityController : GeneralController<University>
     {
-        private readonly IUniversityRepository repository;
-        public UniversityController(IUniversityRepository Repository)
-        {
-            repository = Repository;
-        }
-
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var universities = repository.GetAll();
-            
-            if (!universities.Any()) 
-            {
-                return NotFound();
-            }
-
-            return Ok(universities);
-        }
-
-
-        [HttpGet("{guid}")]
-        public IActionResult GetByGuid(Guid guid) 
-        {
-            var university = repository.GetByGuid(guid);
-            if (university is null) 
-            {
-                return NotFound(); 
-            }
-
-            return Ok(university);
-        }
-
-
-        [HttpPost]
-        public IActionResult Create(University university) 
-        {
-            var createdUniversity = repository.Create(university);
-            return Ok(createdUniversity);
-        }
-
-
-        [HttpPut]
-        public IActionResult Update(University university)
-        {
-            var isUpdated = repository.Update(university);
-            if (!isUpdated)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
-
-
-        [HttpDelete]
-        public IActionResult Delete(Guid guid)
-        {
-            var isDeleted = repository.Delete(guid);
-            if(!isDeleted)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
+        public UniversityController(IUniversityRepository Repository) : base(Repository) { }
     }
 }
