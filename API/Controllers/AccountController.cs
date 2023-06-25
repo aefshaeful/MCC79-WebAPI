746ee95e6a1c7bb2ffb1d783d35d2e075fa0c6a2
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 09:17:44 +0700

    Update AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,76 +7,8 @@ namespace API.Controllers
     [ApiController]
     [Route("api/account")]
 
-    public class AccountController : ControllerBase
+    public class AccountController : GeneralController<Account>
     {
-        private readonly IAccountRepository repository;
-        public AccountController(IAccountRepository Repository)
-        {
-            this.repository = Repository;
-        }
-
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var account = repository.GetAll();
-
-            if (!account.Any())
-            {
-                return NotFound();
-            }
-
-            return Ok(account);
-        }
-
-
-        [HttpGet("{guid}")]
-        public IActionResult GetByGuid(Guid guid)
-        {
-            var account = repository.GetByGuid(guid);
-
-            if (account is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(account);
-        }
-
-
-        [HttpPost]
-        public IActionResult Create(Account account)
-        {
-            var createdAccount = repository.Create(account);
-            return Ok(createdAccount);
-        }
-
-
-        [HttpPut]
-        public IActionResult Update(Account account)
-        {
-            var isUpdate = repository.Update(account);
-
-            if (!isUpdate)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
-
-
-        [HttpDelete]
-        public IActionResult Delete(Guid guid)
-        {
-            var isDelete = repository.Delete(guid);
-
-            if (!isDelete)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
+        public AccountController(IAccountRepository Repository) : base(Repository) { }
     }
 }
