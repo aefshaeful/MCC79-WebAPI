fbb7881a3bd67e0085eba76cf5ea05e70a69e657
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 09:18:17 +0700

    Update EmployeeController.cs

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -8,71 +8,8 @@ namespace API.Controllers
     [ApiController]
     [Route("api/employee")]
 
-    public class EmployeeController : ControllerBase
+    public class EmployeeController : GeneralController<Employee>
     {
-        private readonly IEmployeeRepository repository;
-        public EmployeeController(IEmployeeRepository Repository) 
-        {
-            this.repository = Repository;
-        }
-
-        [HttpGet]
-        public IActionResult GetAll() 
-        {
-            var employee = repository.GetAll();
-
-            if (!employee.Any())
-            {
-                return NotFound();
-            }
-
-            return Ok(employee);
-        }
-
-        [HttpGet("{guid}")]
-        public IActionResult GetByGuid(Guid guid)
-        {
-            var employee = repository.GetByGuid(guid);
-            
-            if(employee is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(employee);
-        }
-
-        [HttpPost]
-        public IActionResult Create(Employee employee) 
-        {
-            var createdEmployee = repository.Create(employee);
-            return Ok(createdEmployee);
-        }
-
-        [HttpPut]
-        public IActionResult Update(Employee employee) 
-        {
-            var isUpdate = repository.Update(employee);
-
-            if (!isUpdate) 
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
-
-        [HttpDelete]
-        public IActionResult Delete(Guid guid)
-        {
-            var isDelete = repository.Delete(guid);
-
-            if(!isDelete)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
+        public EmployeeController(IEmployeeRepository Repository): base(Repository) { }
     }
 }
