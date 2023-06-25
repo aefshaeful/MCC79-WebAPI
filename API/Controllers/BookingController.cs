4c1f8bbe7f084b74346f5344911695cf44485702
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 09:17:58 +0700

    Update BookingController.cs

--- API/Controllers/BookingController.cs
+++ API/Controllers/BookingController.cs
@@ -7,76 +7,8 @@ namespace API.Controllers
     [ApiController]
     [Route("api/booking")]
 
-    public class BookingController : ControllerBase
+    public class BookingController : GeneralController<Booking>
     {
-        private readonly IBookingRepository repository;
-        public BookingController(IBookingRepository Repository)
-        {
-            this.repository = Repository;
-        }
-
-
-        [HttpGet]
-        public IActionResult GettAll()
-        {
-            var booking = repository.GetAll();
-
-            if (!booking.Any())
-            {
-                return NotFound();
-            }
-
-            return Ok(booking);
-        }
-
-
-        [HttpGet("{guid}")]
-        public IActionResult GetByGuid(Guid guid)
-        {
-            var booking = repository.GetByGuid(guid);
-
-            if (booking is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(booking);
-        }
-
-
-        [HttpPost]
-        public IActionResult Create(Booking booking) 
-        {
-            var createdBooking = repository.Create(booking);
-            return Ok(createdBooking);
-        }
-
-
-        [HttpPut]
-        public IActionResult Update(Booking booking) 
-        {
-            var isUpdate = repository.Update(booking);
-           
-            if(!isUpdate)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
-
-
-        [HttpDelete]
-        public IActionResult Delete(Guid guid)
-        {
-            var isDelete = repository.Delete(guid);
-
-            if(!isDelete)
-            {
-                return NotFound();
-            }
-
-            return Ok();
-        }
+        public BookingController(IBookingRepository Repository) : base(Repository) { }
     }
 }
