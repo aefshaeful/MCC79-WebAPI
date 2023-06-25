ea9e46ddea1990d2b257eec910c55631c77951f5
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-25 08:33:25 +0700

    Update BookingRepository.cs

--- API/Repositories/BookingRepository.cs
+++ API/Repositories/BookingRepository.cs
@@ -4,70 +4,8 @@ using API.Models;
 
 namespace API.Repositories
 {
-    public class BookingRepository : IBookingRepository
+    public class BookingRepository : GeneralRepository<Booking>, IBookingRepository
     {
-        private readonly BookingDbContext context;
-        public BookingRepository (BookingDbContext Context) 
-        {
-            this.context = Context;
-        }
-
-        public ICollection<Booking> GetAll() 
-        {
-            return context.Set<Booking>().ToList();
-        }
-
-        public Booking? GetByGuid(Guid guid) 
-        {
-            return context.Set<Booking>().Find(guid);
-        }
-
-        public Booking Create(Booking booking) 
-        {
-            try
-            {
-                context.Set<Booking>().Add(booking);
-                context.SaveChanges();
-                return booking;
-            }
-            catch (Exception ex) 
-            {
-                return new Booking();
-            }
-        }
-
-        public bool Update(Booking booking) 
-        {
-            try
-            {
-                context.Set<Booking>().Update(booking);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex) 
-            {
-                return false;
-            }
-        }
-
-        public bool Delete(Guid guid)
-        {
-            try
-            {
-                var booking = GetByGuid(guid);
-                
-                if (booking is null) 
-                {
-                    return false;
-                }
-                context.Set<Booking>().Remove(booking);
-                context.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
+        public BookingRepository(BookingDbContext Context) : base(Context) { }
     }
 }
