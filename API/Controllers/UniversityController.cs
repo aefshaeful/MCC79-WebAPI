2e6871b7feceeceb8059cefd2277f299308d50d4
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-26 13:00:15 +0700

    Update UniversityController.cs

--- API/Controllers/UniversityController.cs
+++ API/Controllers/UniversityController.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using API.Models;
 using API.Contracts;
-
+using System.Net;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/universities")]
-    public class UniversityController : GeneralController<University>
+    public class UniversityController : GeneralController<IUniversityRepository, University> 
     {
         public UniversityController(IUniversityRepository Repository) : base(Repository) { }
+
+
+        [HttpGet("get-by-name/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var university = repository.GetByName(name);
+            if (university is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(university);
+
+            /*var university = repository.GetByName(name);
+            if (!university.Any())
+            {
+                return NotFound(new ResponsHandler<University> 
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "No Universities found with the given name"
+                })
+            }
+
+            return Ok(new ResponsHandler<IEnumerable<University>)
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.
+            }*/
+        }
     }
 }
