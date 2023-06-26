27d4461c4b35f564082e351e98825499c5082542
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-26 12:59:38 +0700

    Update AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,13 +1,14 @@
 ﻿using API.Models;
 using API.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using API.Repositories;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/account")]
 
-    public class AccountController : GeneralController<Account>
+    public class AccountController : GeneralController<IAccountRepository, Account>
     {
         public AccountController(IAccountRepository Repository) : base(Repository) { }
     }
