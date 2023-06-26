fe5ad55acf116862954df3363f06a0fef5be8d79
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-26 15:08:17 +0700

    Data Transfer Object

---
+++ API/Services/UniversityServices.cs
@@ -0,0 +1,32 @@
+﻿using API.Contracts;
+
+namespace API.Services
+{
+    public class UniversityServices
+    {
+       /* private readonly IUniversityRepository _universityrepository;
+
+        public UniversityServices(IUniversityRepository universityrepository)
+        {
+            _universityrepository = universityrepository;
+        }
+
+        public IEnumrable<GetUniversityDto>? GetUniversity()
+        {
+            var universities = _universityrepository.GetAll();
+            if (!universities.Any()) 
+            {
+                return null;
+            }
+
+            var toDto = universities.Select(university => new GetUniversityDto 
+            { 
+                Guid = university.Guid,
+                Code = university.Code,
+                Name = university.Name
+            }).ToList();
+
+            return toDto;
+        }*/
+    }
+}
