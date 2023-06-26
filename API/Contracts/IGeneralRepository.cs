9198c0286992aef54ddce52bbdb06bc5f181b8ea
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-26 12:59:32 +0700

    Update IGeneralRepository.cs

--- API/Contracts/IGeneralRepository.cs
+++ API/Contracts/IGeneralRepository.cs
@@ -1,12 +1,9 @@
-﻿using System.Xml.Linq;
-
-namespace API.Contracts
+﻿namespace API.Contracts
 {
     public interface IGeneralRepository<TEntity>
     {
         ICollection<TEntity> GetAll();
         TEntity? GetByGuid(Guid guid);
-        TEntity? GetByName(string name);
         TEntity? Create(TEntity entity);
         bool Update(TEntity entity);
         bool Delete(Guid guid);
