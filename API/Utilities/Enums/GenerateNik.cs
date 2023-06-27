9792110a6e697cb83aab6c049898c5f9ba2af5bc
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-27 17:56:21 +0700

    Create GenerateNik.cs

---
+++ API/Utilities/Enums/GenerateNik.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Utilities.Enums
+{
+    public class GenerateNik : ValidationAttribute
+    {
+        /*public override string IsValid(object? value)
+        {
+            var nik = (string)value;
+            if (nik is null)
+            {
+                return "11111";
+            }
+            else
+            {
+                string lastNik = GetLastEmployeeNik();
+                int nextNik = int.Parse(lastNik) + 1;
+                return nextNik.ToString();
+            }
+        }*/
+    }
+}
