7efb296d65ef24d407bfbb0f9a307c423a731c7a
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-01 17:51:17 +0700

    Password Confirmation Validation

---
+++ API/Utilities/Enums/ConfirmPasswordAttribute.cs
@@ -0,0 +1,37 @@
+﻿using API.Contracts;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+
+namespace API.Utilities.Enums
+{
+    public class ConfirmPasswordAttribute : ValidationAttribute
+    {
+        private readonly string _password;
+
+        public ConfirmPasswordAttribute(string password)
+        {
+            _password = password;
+        }
+
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var passwordProperty = validationContext.ObjectType.GetProperty(_password);
+            {
+                if (passwordProperty != null) 
+                {
+                    var passwordValue = passwordProperty.GetValue(validationContext.ObjectInstance, null);
+
+                    if (value != null && passwordValue != null && !value.Equals(passwordValue)) 
+                    {
+                        return new ValidationResult(ErrorMessage);
+                    }
+                }
+
+                return ValidationResult.Success;
+            }
+
+        }
+    }
+}
