8d7512402c807bbdf58e4a70a82be29f67fabb49
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-01 17:55:24 +0700

    Create LoginDto.cs

---
+++ API/DTOs/Account/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Utilities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Account
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [PasswordPolicy]
+        public string Password { get; set; }
+    }
+}
